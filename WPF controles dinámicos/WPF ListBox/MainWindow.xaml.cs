09c5d17a2935fe3a2c75aaba6e593526e4c3d81b
sbaezfedu <45758415+sbaezfedu@users.noreply.github.com>
2023-02-21 20:48:06 +0100

    Nuevo proyecto

---
+++ WPF controles dinámicos/WPF ListBox/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_ListBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Lista desde el código C# que se añade manualmente a la propiedad ItemsSource de un ListBox
+            List<TodoItem> items = new List<TodoItem>();
+            items.Add(new TodoItem() { Title = "WPF", Completion = 45 });
+            items.Add(new TodoItem() { Title = "C#", Completion = 80 });
+            items.Add(new TodoItem() { Title = "Java", Completion = 0 });
+            this.lbTodoList.ItemsSource = items;
+        }
+    }
+
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public int Completion { get; set; }
+
+        // Sobrescribimos el método ToString
+        public override string ToString()
+        {
+            return this.Title + "-" + this.Completion;
+        }
+    }
+}
---
+++ WPF controles dinámicos/WPF ListView/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_ListView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Para ordenar la lista se tiene que configurar desde el código C#
+            CollectionView view = (CollectionView) CollectionViewSource.GetDefaultView(lvUsers.ItemsSource);
+            view.SortDescriptions.Add(new SortDescription("Age", ListSortDirection.Ascending));
+
+            view.Filter = UserFilter;
+        }
+
+        private bool UserFilter(object item)
+        {
+            if (String.IsNullOrEmpty(this.txtFilter.Text))
+                return true;
+            else
+                return ((item as User).Name.IndexOf(this.txtFilter.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+        }
+
+        private void txtFilter_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(lvUsers.ItemsSource).Refresh();
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Mail { get; set; }
+    }
+}
